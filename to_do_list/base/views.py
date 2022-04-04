8bfca615bf9b8108696befde8c0ee6fb6f7252ab
pratyush2109 <pratyush.ambastha21@gmail.com>
2022-04-05 00:42:12 +0530

    Model_creation_and_task_page

--- to_do_list/base/views.py
+++ to_do_list/base/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
-def taskList(request):
-    return HttpResponse('To Do list')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    
